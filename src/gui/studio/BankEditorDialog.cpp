8e5a2926cc6163547756129c17e4879da8eeaeb9
Ted Felix <ted@tedfelix.com>
2014-07-20 12:27:13 +0000

    qt5: Change QDesktopServices -> QStandardPaths
    ...in Qt5 builds only.  Patch from Tim Munro.
    
    
    git-svn-id: file:///home/ted/devel/rosegarden-svn-backup/code/trunk/rosegarden@13742 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/gui/studio/BankEditorDialog.cpp
+++ src/gui/studio/BankEditorDialog.cpp
@@ -67,6 +67,9 @@
 #include <QHBoxLayout>
 #include <QShortcut>
 #include <QDesktopServices>
+#if QT_VERSION >= 0x050000
+#include <QStandardPaths>
+#endif
 #include <QDialogButtonBox>
 #include <QSettings>
 
@@ -1554,7 +1557,11 @@ BankEditorDialog::addCommandToHistory(Command *command)
 void
 BankEditorDialog::slotImport()
 {
+#if QT_VERSION >= 0x050000
+    QString home = QUrl::fromLocalFile(QStandardPaths::writableLocation(QStandardPaths::HomeLocation)).path();
+#else
     QString home = QUrl::fromLocalFile(QDesktopServices::storageLocation(QDesktopServices::HomeLocation)).path();
+#endif
     QString deviceDir = home + "/.local/share/rosegarden/library";
 
     QString url_str = FileDialog::getOpenFileName(this, tr("Import Banks from Device in File"), deviceDir,
