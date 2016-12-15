f04628c3c5d0115f9197a9b36a8ac470fe4be761
Ted Felix <ted@tedfelix.com>
2016-12-15 00:04:35 +0000

    Promote QProgressBar style to rosegarden.qss
    This fixes the broken style that was in rosegarden.qss.
    
    
    git-svn-id: file:///home/ted/devel/rosegarden-svn-backup/code/trunk/rosegarden@14797 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/gui/widgets/ProgressBar.cpp
+++ src/gui/widgets/ProgressBar.cpp
@@ -34,14 +34,6 @@ ProgressBar::ProgressBar(/*int totalSteps, */
 {
 /*    setRange(0, totalSteps); */
 
-    QSettings settings;
-    settings.beginGroup(GeneralOptionsConfigGroup);
-    bool Thorn = settings.value("use_thorn_style", true).toBool();
-    settings.endGroup();
-
-    QString localStyle("QProgressBar { color: #000000; background: #FFFFFF; border: 1px solid #AAAAAA; border-radius: 3px; }  QProgressBar::chunk { background-color: #D6E8FB; width: 20px; }");
-    if (Thorn) setStyleSheet(localStyle);
-
     connect (this, SIGNAL(valueChanged(int)), this, SLOT(WTF(int)));
 }
 
@@ -50,14 +42,6 @@ ProgressBar::ProgressBar(int totalSteps,
          QProgressBar(parent)
 {
     setRange(0, totalSteps);
-
-    QSettings settings;
-    settings.beginGroup(GeneralOptionsConfigGroup);
-    bool Thorn = settings.value("use_thorn_style", true).toBool();
-    settings.endGroup();
-
-    QString localStyle("QProgressBar { color: #000000; background: #FFFFFF; border: 1px solid #AAAAAA; border-radius: 3px; }  QProgressBar::chunk { background-color: #D6E8FB; width: 20px; }");
-    if (Thorn) setStyleSheet(localStyle);
 
     connect (this, SIGNAL(valueChanged(int)), this, SLOT(WTF(int)));
 }
