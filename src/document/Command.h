0161a3e6e65ab6f1cface87ba7890d0c20aeb8f8
D. Michael McIntyre <michael.mcintyre@rosegardenmusic.com>
2017-01-09 16:23:48 +0000

    Update copyright for 2017
    git-svn-id: file:///home/ted/devel/rosegarden-svn-backup/code/trunk/rosegarden@14869 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/document/Command.h
+++ src/document/Command.h
@@ -3,14 +3,7 @@
 /*
     Rosegarden
     A MIDI and audio sequencer and musical notation editor.
-
-    This program is Copyright 2000-2016
-        Guillaume Laurent   <glaurent@telegraph-road.org>,
-        Chris Cannam        <cannam@all-day-breakfast.com>,
-        Richard Bown        <richard.bown@ferventsoftware.com>
-
-    The moral rights of Guillaume Laurent, Chris Cannam, and Richard
-    Bown to claim authorship of this work have been asserted.
+    Copyright 2000-2017 the Rosegarden development team.
 
     Other copyrights also apply to some parts of this work.  Please
     see the AUTHORS file and individual file headers for details.
@@ -20,6 +13,7 @@
     published by the Free Software Foundation; either version 2 of the
     License, or (at your option) any later version.  See the file
     COPYING included with this distribution for more information.
+    Rosegarden
 */
 
 #ifndef RG_COMMAND_H
--- src/sound/AlsaDriver.cpp
+++ src/sound/AlsaDriver.cpp
@@ -3,7 +3,7 @@
 /*
   Rosegarden
   A sequencer and musical notation editor.
-  Copyright 2000-2016 the Rosegarden development team.
+  Copyright 2000-2017 the Rosegarden development team.
   See the AUTHORS file for more details.
  
   This program is free software; you can redistribute it and/or
