45b89c73d4cff99b104adb12678c30d8c38bf5f9
Ted Felix <ted@tedfelix.com>
2018-04-19 02:12:22 +0000

    Comments
    git-svn-id: file:///home/ted/devel/rosegarden-svn-backup/code/trunk/rosegarden@15252 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/sound/Audit.h
+++ src/sound/Audit.h
@@ -17,13 +17,32 @@
 #define RG_AUDIT_H
 
 #include <string>
-
 #include <sstream>
-
-// A staggeringly simple-minded audit trail implementation.
 
 namespace Rosegarden {
 
+
+/// Logging class that makes it easier for users to get debug info.
+/**
+ * This is used at startup to log information that could be helpful for
+ * diagnosing certain problems related to audio and MIDI.  The contents
+ * of this log can be examined by the user via the preferences dialog.
+ * Edit > Preferences... > General tab > Details... button.
+ *
+ * There are some issues with this class as it stands:
+ *
+ *   1. It batches the logging and sends it to RG_DEBUG on destruction
+ *      of an Audit instance.  This means that if there is a crash,
+ *      important debug output will be lost.
+ *
+ *   2. It uses std::stringstream which means it cannot take advantage
+ *      of the insertion operators that have been coded for QDebug.
+ *
+ * Recommendation: Remove the RG_DEBUG from the dtor.
+ *   Then use Audit in parallel with RG_DEBUG/RG_WARNING.  It's duplicated
+ *   code, but it solves the key issue which is the loss of logging in the
+ *   case of a crash.
+ */
 class Audit : public std::stringstream
 {
 public:
@@ -36,6 +55,7 @@ public:
 protected:
     static std::string m_audit;
 };
+
 
 }
 
