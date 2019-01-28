4cad28dc1867aeae0d09d2f230664728952e8f0f
Ted Felix <ted@tedfelix.com>
2019-01-28 01:40:39 +0000

    Enable High DPI Scaling
    ...via Qt::AA_EnableHighDpiScaling.  Bug #1565.
    Patch by Hubert Figuiere.
    
    
    git-svn-id: file:///home/ted/devel/rosegarden-svn-backup/code/trunk/rosegarden@15444 cf6a83e7-f017-4305-b0b3-678bde24f230

--- src/gui/application/main.cpp
+++ src/gui/application/main.cpp
@@ -383,6 +383,8 @@ int main(int argc, char *argv[])
         }
     }
 
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     RosegardenApplication theApp(argc, argv);
 
     theApp.setOrganizationName("rosegardenmusic");
