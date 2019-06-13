adfaa08a87f6ae55f8b84d5e1d2a9ae001080396
Ted Felix <ted@tedfelix.com>
2019-06-13 16:05:00 +0000

    Add AutoScroller class with new FollowMode typedef
    git-svn-id: file:///home/ted/devel/rosegarden-svn-backup/code/trunk/rosegarden@15533 cf6a83e7-f017-4305-b0b3-678bde24f230

---
+++ src/gui/general/AutoScroller.h
@@ -0,0 +1,54 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*- vi:set ts=8 sts=4 sw=4: */
+
+/*
+    Rosegarden
+    A MIDI and audio sequencer and musical notation editor.
+    Copyright 2000-2018 the Rosegarden development team.
+
+    Other copyrights also apply to some parts of this work.  Please
+    see the AUTHORS file and individual file headers for details.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.  See the file
+    COPYING included with this distribution for more information.
+*/
+
+#ifndef RG_AUTOSCROLLER_H
+#define RG_AUTOSCROLLER_H
+
+#include <QObject>
+
+namespace Rosegarden
+{
+
+
+typedef int FollowMode;
+constexpr int NO_FOLLOW = 0;  // Was NoFollow
+constexpr int FOLLOW_HORIZONTAL = 1;  // Was FollowHorizontal
+constexpr int FOLLOW_VERTICAL = 2;  // Was FollowVertical
+
+/// Handle auto-scroll behavior when the mouse is dragged outside a window.
+/**
+ * Currently, this behavior is spread around the system in doAutoScroll()
+ * routines.  This class should be able to eliminate most of that copy/paste
+ * code.
+ *
+ * See RosegardenScrollView::doAutoScroll(), MatrixWidget::doAutoScroll(),
+ * and the upcoming NotationWidget::doAutoScroll().
+ */
+class AutoScroller : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    // To Be Implemented...
+
+};
+
+
+}
+
+#endif
