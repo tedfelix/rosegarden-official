bc1a3742aedd0b824a1c2fa643c37c4ee83275d7
Ted Felix <ted@tedfelix.com>
2020-05-04 01:24:43 +0000

    Standards: Tabs -> Spaces
    git-svn-id: file:///home/ted/devel/rosegarden-svn-backup/code/trunk/rosegarden@15822 cf6a83e7-f017-4305-b0b3-678bde24f230

--- test/base/seq/complainer.c
+++ test/base/seq/complainer.c
@@ -10,24 +10,24 @@ callback(snd_seq_t *handle)
     snd_seq_event_t *ev = 0;
 
     do {
-	if (snd_seq_event_input(handle, &ev) > 0) {
+        if (snd_seq_event_input(handle, &ev) > 0) {
 
-	    if (ev->type == SND_SEQ_EVENT_NOTEON) {
+            if (ev->type == SND_SEQ_EVENT_NOTEON) {
 
-		struct timeval tv;
-		static long last_usec = 0;
-		int pitch = ev->data.note.note;
+                struct timeval tv;
+                static long last_usec = 0;
+                int pitch = ev->data.note.note;
 
-		snd_seq_timestamp_t evt = ev->time;
+                snd_seq_timestamp_t evt = ev->time;
 
-		gettimeofday(&tv, 0);
-		printf("pitch %d at %ld sec %ld usec, off by %ld usec\n",
-		       pitch, tv.tv_sec, tv.tv_usec, tv.tv_usec - ((last_usec + 500000) % 1000000));
+                gettimeofday(&tv, 0);
+                printf("pitch %d at %ld sec %ld usec, off by %ld usec\n",
+                        pitch, tv.tv_sec, tv.tv_usec, tv.tv_usec - ((last_usec + 500000) % 1000000));
 
-		last_usec = tv.tv_usec;
-	    }
-	}
-	
+                last_usec = tv.tv_usec;
+            }
+        }
+
     } while (snd_seq_event_input_pending(handle, 0) > 0);
 }
 
@@ -41,17 +41,17 @@ main(int argc, char **argv)
     struct sched_param param;
 
     if (snd_seq_open(&handle, "hw", SND_SEQ_OPEN_DUPLEX, 0) < 0) {
-	fprintf(stderr, "failed to open ALSA sequencer interface\n");
-	return 1;
+        fprintf(stderr, "failed to open ALSA sequencer interface\n");
+        return 1;
     }
 
     snd_seq_set_client_name(handle, "complainer");
 
     if ((portid = snd_seq_create_simple_port
-	 (handle, "complainer",
-	  SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE, 0)) < 0) {
-	fprintf(stderr, "failed to create ALSA sequencer port\n");
-	return 1;
+            (handle, "complainer",
+                    SND_SEQ_PORT_CAP_WRITE | SND_SEQ_PORT_CAP_SUBS_WRITE, 0)) < 0) {
+        fprintf(stderr, "failed to create ALSA sequencer port\n");
+        return 1;
     }
 
     npfd = snd_seq_poll_descriptors_count(handle, POLLIN);
@@ -60,15 +60,15 @@ main(int argc, char **argv)
 
     param.sched_priority = 99;
     if (sched_setscheduler(0, SCHED_FIFO, &param)) {
-	perror("failed to set high-priority scheduler");
+        perror("failed to set high-priority scheduler");
     }
 
     printf("ready... npfd: %d\n", npfd);
 
     while (1) {
-	if (poll(pfd, npfd, 100000) > 0) {
-	    callback(handle);
-	}  
+        if (poll(pfd, npfd, 100000) > 0) {
+            callback(handle);
+        }
     }
 }
 
--- test/base/seq/queue-timer.c
+++ test/base/seq/queue-timer.c
@@ -6,14 +6,14 @@
 void normalize(struct timeval *tv)
 {
     if (tv->tv_sec == 0) {
-	while (tv->tv_usec <= -1000000) { tv->tv_usec += 1000000; --tv->tv_sec; }
-	while (tv->tv_usec >=  1000000) { tv->tv_usec -= 1000000; ++tv->tv_sec; }
+        while (tv->tv_usec <= -1000000) { tv->tv_usec += 1000000; --tv->tv_sec; }
+        while (tv->tv_usec >=  1000000) { tv->tv_usec -= 1000000; ++tv->tv_sec; }
     } else if (tv->tv_sec < 0) {
-	while (tv->tv_usec <= -1000000) { tv->tv_usec += 1000000; --tv->tv_sec; }
-	while (tv->tv_usec > 0) { tv->tv_usec -= 1000000; ++tv->tv_sec; }
+        while (tv->tv_usec <= -1000000) { tv->tv_usec += 1000000; --tv->tv_sec; }
+        while (tv->tv_usec > 0) { tv->tv_usec -= 1000000; ++tv->tv_sec; }
     } else { 
-	while (tv->tv_usec >= 1000000) { tv->tv_usec -= 1000000; ++tv->tv_sec; }
-	while (tv->tv_usec < 0) { tv->tv_usec += 1000000; --tv->tv_sec; }
+        while (tv->tv_usec >= 1000000) { tv->tv_usec -= 1000000; ++tv->tv_sec; }
+        while (tv->tv_usec < 0) { tv->tv_usec += 1000000; --tv->tv_sec; }
     }
 }
 
@@ -33,24 +33,24 @@ main(int argc, char **argv)
     snd_timer_id_t *timerid;
 
     if (snd_seq_open(&handle, "hw", SND_SEQ_OPEN_DUPLEX, 0) < 0) {
-	fprintf(stderr, "failed to open ALSA sequencer interface\n");
-	return 1;
+        fprintf(stderr, "failed to open ALSA sequencer interface\n");
+        return 1;
     }
 
     snd_seq_set_client_name(handle, "generator");
 
     if ((portid = snd_seq_create_simple_port
-	 (handle, "generator",
-	  SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SUBS_READ, 0)) < 0) {
-	fprintf(stderr, "failed to create ALSA sequencer port\n");
-	return 1;
+            (handle, "generator",
+                    SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SUBS_READ, 0)) < 0) {
+        fprintf(stderr, "failed to create ALSA sequencer port\n");
+        return 1;
     }
 
     if ((queue = snd_seq_alloc_queue(handle)) < 0) {
-	fprintf(stderr, "failed to create ALSA sequencer queue\n");
-	return 1;
+        fprintf(stderr, "failed to create ALSA sequencer queue\n");
+        return 1;
     }
-/*
+    /*
     snd_seq_queue_timer_alloca(&timer);
     snd_seq_get_queue_timer(handle, queue, timer);
     snd_timer_id_alloca(&timerid);
@@ -61,7 +61,7 @@ main(int argc, char **argv)
     snd_timer_id_set_subdevice(timerid, 0);
     snd_seq_queue_timer_set_id(timer, timerid);
     snd_seq_set_queue_timer(handle, queue, timer);
-*/
+     */
     snd_seq_start_queue(handle, queue, 0);
     snd_seq_drain_output(handle);
 
@@ -71,53 +71,53 @@ main(int argc, char **argv)
 
     while (countdown != 0) {
 
-	snd_seq_queue_status_t *status;
-	const snd_seq_real_time_t *rtime;
-	struct timeval tv, diff, diffdiff;
+        snd_seq_queue_status_t *status;
+        const snd_seq_real_time_t *rtime;
+        struct timeval tv, diff, diffdiff;
 
-	snd_seq_queue_status_alloca(&status);
+        snd_seq_queue_status_alloca(&status);
 
-	snd_seq_get_queue_status(handle, queue, status);
-	rtime = snd_seq_queue_status_get_real_time(status);
+        snd_seq_get_queue_status(handle, queue, status);
+        rtime = snd_seq_queue_status_get_real_time(status);
 
-	gettimeofday(&tv, 0);
+        gettimeofday(&tv, 0);
 
-	tv.tv_sec -= starttv.tv_sec;
-	tv.tv_usec -= starttv.tv_usec;
-	normalize(&tv);
+        tv.tv_sec -= starttv.tv_sec;
+        tv.tv_usec -= starttv.tv_usec;
+        normalize(&tv);
 
-	diff.tv_sec = tv.tv_sec - rtime->tv_sec;
-	diff.tv_usec = tv.tv_usec - rtime->tv_nsec / 1000;
-	normalize(&diff);
+        diff.tv_sec = tv.tv_sec - rtime->tv_sec;
+        diff.tv_usec = tv.tv_usec - rtime->tv_nsec / 1000;
+        normalize(&diff);
 
-	diffdiff.tv_sec = diff.tv_sec - prevdiff.tv_sec;
-	diffdiff.tv_usec = diff.tv_usec - prevdiff.tv_usec;
-	normalize(&diffdiff);
-	prevdiff = diff;
+        diffdiff.tv_sec = diff.tv_sec - prevdiff.tv_sec;
+        diffdiff.tv_usec = diff.tv_usec - prevdiff.tv_usec;
+        normalize(&diffdiff);
+        prevdiff = diff;
 
-	fprintf(stderr, " real time: %12ld sec %8ld usec\nqueue time: %12u sec %8u usec\n      diff: %12ld sec %8ld usec\n  diffdiff: %12ld sec %8ld usec\n",
-		tv.tv_sec, tv.tv_usec,
-		rtime->tv_sec, rtime->tv_nsec / 1000,
-		diff.tv_sec, diff.tv_usec,
-		diffdiff.tv_sec, diffdiff.tv_usec);
+        fprintf(stderr, " real time: %12ld sec %8ld usec\nqueue time: %12u sec %8u usec\n      diff: %12ld sec %8ld usec\n  diffdiff: %12ld sec %8ld usec\n",
+                tv.tv_sec, tv.tv_usec,
+                rtime->tv_sec, rtime->tv_nsec / 1000,
+                diff.tv_sec, diff.tv_usec,
+                diffdiff.tv_sec, diffdiff.tv_usec);
 
-	if (diffdiff.tv_usec >  5000 ||
-	    diffdiff.tv_usec < -5000) {
-	    fprintf(stderr, "oops! queue slipped\n");
-	    if (tv.tv_sec < 5) {
-		fprintf(stderr, "(ignoring in first few seconds)\n");
-	    } else {
-		countdown = 2;
-	    }
-	} else {
-	    if (countdown > 0) --countdown;
-	}
+        if (diffdiff.tv_usec >  5000 ||
+                diffdiff.tv_usec < -5000) {
+            fprintf(stderr, "oops! queue slipped\n");
+            if (tv.tv_sec < 5) {
+                fprintf(stderr, "(ignoring in first few seconds)\n");
+            } else {
+                countdown = 2;
+            }
+        } else {
+            if (countdown > 0) --countdown;
+        }
 
-	fprintf(stderr, "\n");
-	struct timespec ts;
-	ts.tv_sec = 1;
-	ts.tv_nsec = 0;
-	nanosleep(&ts, 0);
+        fprintf(stderr, "\n");
+        struct timespec ts;
+        ts.tv_sec = 1;
+        ts.tv_nsec = 0;
+        nanosleep(&ts, 0);
     }
 }
 
