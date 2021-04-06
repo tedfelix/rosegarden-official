6ef310100dc5b236ba7a9a9f35905fa39821a476
Ted Felix <ted@tedfelix.com>
2021-04-06 15:37:26 -0400

    cmake: Add git rev hash to BUILDKEY

--- src/buildkey.cmake
+++ src/buildkey.cmake
@@ -15,12 +15,14 @@ if (NOT RG_BUILDKEY)
    set(RG_BUILDKEY "Error")
 endif()
 
+#[[
+
+# Old svn stuff.
+
 # Magic for getting the SVN revision at build time
 # http://stackoverflow.com/questions/3780667/use-cmake-to-get-build-time-svn-revision
 # combined with
 # http://stackoverflow.com/questions/17451226/cmake-how-to-get-subversion-revision-but-not-to-fail-if-we-made-an-export?s=1%7C0.1607
-
-#[[
 
 # the FindSubversion.cmake module is part of the standard distribution
 include(FindSubversion)
@@ -47,7 +49,33 @@ if(Subversion_FOUND)
       endif()
    endif()
 endif()
+
 ]]
+
+find_program(GIT_EXE git)
+if (GIT_EXE)
+   # Get the git commit hash.
+   execute_process(COMMAND ${GIT_EXE} rev-parse --short HEAD
+      OUTPUT_VARIABLE MY_WC_REVISION
+      ERROR_VARIABLE git_rev_parse_error
+      RESULT_VARIABLE git_rev_parse_result
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+   # git status --porcelain
+   execute_process(COMMAND ${GIT_EXE} status --porcelain
+      OUTPUT_VARIABLE MY_WC_STATUS
+      ERROR_VARIABLE git_status_error
+      RESULT_VARIABLE git_status_result
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+   # Working copy not clean?  Add a "*" to the revision.
+   if (NOT ${MY_WC_STATUS} STREQUAL "")
+      SET(MY_WC_REVISION "${MY_WC_REVISION}*")
+   endif()
+
+   #message(STATUS "git revision ${MY_WC_REVISION}")
+   SET(RG_BUILDKEY "${RG_BUILDKEY} (git: ${MY_WC_REVISION})")
+endif()
 
 message(STATUS "Build key ${RG_BUILDKEY}")
 
