c325cd33224df1b1add6c5ebf834888800a515d0
philip <leishman@web.de>
2021-04-22 15:16:22 +0200

    put back return in Segment debug

--- src/base/Segment.cpp
+++ src/base/Segment.cpp
@@ -1785,7 +1785,7 @@ ROSEGARDENPRIVATE_EXPORT QDebug &operator<<(QDebug &dbg, const Rosegarden::Segme
             continue;
         }
 
-        dbg << *(*i);
+        dbg << *(*i) << "\n";
     }
 
     return dbg;
