f506be1804fe69aa06604a08819a8563dbb4998a
Ted Felix <ted@tedfelix.com>
2021-06-23 21:59:58 -0400

    Cleanup

--- test/lilypond/lilypond_export_test.cpp
+++ test/lilypond/lilypond_export_test.cpp
@@ -29,6 +29,7 @@ private Q_SLOTS:
     void testEmptyDocument();
 
     // Data-driven testing against the example .rg files.
+    // https://doc.qt.io/qt-5/qttestlib-tutorial2-example.html
     // Generate the test data.
     void testExamples_data();
     // Called for each row of test data.
@@ -139,190 +140,67 @@ Options defaultOptions(ExportBeaming);
 
 void TestLilypondExport::testExamples_data()
 {
-    // Usually "example", but one "regression".
+    // Usually "examples", but one "regression".
     QTest::addColumn<QString>("baseDir");
-    // ??? This is always the row name.  Can we just use the row name?
-    //     And remove this?
-    QTest::addColumn<QString>("baseName");
-    // ??? This is always defaultOptions.  Remove.
-    QTest::addColumn<Options>("options");
 
-    QTest::newRow("aveverum") <<
-            "examples" <<
-            "aveverum" <<
-            defaultOptions;
-    QTest::newRow("aylindaamiga") <<
-            "examples" <<
-            "aylindaamiga" <<
-            defaultOptions;
-    QTest::newRow("bogus-surf-jam") <<
-            "examples" <<
-            "bogus-surf-jam" <<
-            defaultOptions;
-    QTest::newRow("beaming") <<
-            "examples" <<
-            "beaming" <<
-            defaultOptions;
-    QTest::newRow("Brandenburg_No3-BWV_1048") <<
-            "examples" <<
-            "Brandenburg_No3-BWV_1048" <<
-            defaultOptions;
-    QTest::newRow("bwv-1060-trumpet-duet-excerpt") <<
-            "examples" <<
-            "bwv-1060-trumpet-duet-excerpt" <<
-            defaultOptions;
+    QTest::newRow("aveverum") << "examples";
+    QTest::newRow("aylindaamiga") << "examples";
+    QTest::newRow("bogus-surf-jam") << "examples";
+    QTest::newRow("beaming") << "examples";
+    QTest::newRow("Brandenburg_No3-BWV_1048") << "examples";
+    QTest::newRow("bwv-1060-trumpet-duet-excerpt") << "examples";
 
     // Those work but are very slow, and they output lots and lots of
     // WARNING: Rosegarden::Exception: "Bad type for Indication model event
     //          (expected indication, found controller)"
-    //QTest::newRow("children") <<
-    //        "examples" <<
-    //        "children" <<
-    //        defaultOptions;
-    //QTest::newRow("Chopin-Prelude-in-E-minor-Aere") <<
-    //        "examples" <<
-    //        "Chopin-Prelude-in-E-minor-Aere" <<
-    //        defaultOptions;
+    //QTest::newRow("children") << "examples";
+    //QTest::newRow("Chopin-Prelude-in-E-minor-Aere") << "examples";
 
-    QTest::newRow("Djer-Fire") <<
-            "examples" <<
-            "Djer-Fire" <<
-            defaultOptions;
-    QTest::newRow("doodle-q") <<
-            "examples" <<
-            "doodle-q" <<
-            defaultOptions;
-    QTest::newRow("exercise_notation") <<
-            "examples" <<
-            "exercise_notation" <<
-            defaultOptions;
-    QTest::newRow("glazunov-for-solo-and-piano-with-cue") <<
-            "examples" <<
-            "glazunov-for-solo-and-piano-with-cue" <<
-            defaultOptions;
-    QTest::newRow("glazunov") <<
-            "examples" <<
-            "glazunov" <<
-            defaultOptions;
-    QTest::newRow("Hallelujah_Chorus_from_Messiah") <<
-            "examples" <<
-            "Hallelujah_Chorus_from_Messiah" <<
-            defaultOptions;
-    QTest::newRow("headers-and-unicode-lyrics") <<
-            "examples" <<
-            "headers-and-unicode-lyrics" <<
-            defaultOptions;
-    QTest::newRow("himno_de_riego") <<
-            "examples" <<
-            "himno_de_riego" <<
-            defaultOptions;
-    QTest::newRow("interpretation-example") <<
-            "examples" <<
-            "interpretation-example" <<
-            defaultOptions;
-    QTest::newRow("let-all-mortal-flesh") <<
-            "examples" <<
-            "let-all-mortal-flesh" <<
-            defaultOptions;
-    QTest::newRow("lilypond-alternative-endings_new-way") <<
-            "examples" <<
-            "lilypond-alternative-endings_new-way" <<
-            defaultOptions;
-    QTest::newRow("lilypond-alternative-endings") <<
-            "examples" <<
-            "lilypond-alternative-endings" <<
-            defaultOptions;
-    QTest::newRow("lilypond-directives") <<
-            "examples" <<
-            "lilypond-directives" <<
-            defaultOptions;
-    QTest::newRow("lilypond-up-down") <<
-            "examples" <<
-            "lilypond-up-down" <<
-            defaultOptions;
-    QTest::newRow("lilypond-staff-groupings") <<
-            "examples" <<
-            "lilypond-staff-groupings" <<
-            defaultOptions;
-    QTest::newRow("lilypond-tied-grace-notes") <<
-            "examples" <<
-            "lilypond-tied-grace-notes" <<
-            defaultOptions;
-    QTest::newRow("logical-segments-4") <<
-            "examples" <<
-            "logical-segments-4" <<
-            defaultOptions;
-    QTest::newRow("mandolin-sonatina") <<
-            "examples" <<
-            "mandolin-sonatina" <<
-            defaultOptions;
-    QTest::newRow("marks-test") <<
-            "examples" <<
-            "marks-test" <<
-            defaultOptions;
-    QTest::newRow("mozart-quartet") <<
-            "examples" <<
-            "mozart-quartet" <<
-            defaultOptions;
-    QTest::newRow("notation-for-string-orchestra-in-D-minor") <<
-            "examples" <<
-            "notation-for-string-orchestra-in-D-minor" <<
-            defaultOptions;
-    QTest::newRow("perfect-moment") <<
-            "examples" <<
-            "perfect-moment" <<
-            defaultOptions;
-    QTest::newRow("ravel-pc-gmaj-adagio") <<
-            "examples" <<
-            "ravel-pc-gmaj-adagio" <<
-            defaultOptions;
-    QTest::newRow("Romanza") <<
-            "examples" <<
-            "Romanza" <<
-            defaultOptions;
+    QTest::newRow("Djer-Fire") << "examples";
+    QTest::newRow("doodle-q") << "examples";
+    QTest::newRow("exercise_notation") << "examples";
+    QTest::newRow("glazunov-for-solo-and-piano-with-cue") << "examples";
+    QTest::newRow("glazunov") << "examples";
+    QTest::newRow("Hallelujah_Chorus_from_Messiah") << "examples";
+    QTest::newRow("headers-and-unicode-lyrics") << "examples";
+    QTest::newRow("himno_de_riego") << "examples";
+    QTest::newRow("interpretation-example") << "examples";
+    QTest::newRow("let-all-mortal-flesh") << "examples";
+    QTest::newRow("lilypond-alternative-endings_new-way") << "examples";
+    QTest::newRow("lilypond-alternative-endings") << "examples";
+    QTest::newRow("lilypond-directives") << "examples";
+    QTest::newRow("lilypond-up-down") << "examples";
+    QTest::newRow("lilypond-staff-groupings") << "examples";
+    QTest::newRow("lilypond-tied-grace-notes") << "examples";
+    QTest::newRow("logical-segments-4") << "examples";
+    QTest::newRow("mandolin-sonatina") << "examples";
+    QTest::newRow("marks-test") << "examples";
+    QTest::newRow("mozart-quartet") << "examples";
+    QTest::newRow("notation-for-string-orchestra-in-D-minor") << "examples";
+    QTest::newRow("perfect-moment") << "examples";
+    QTest::newRow("ravel-pc-gmaj-adagio") << "examples";
+    QTest::newRow("Romanza") << "examples";
 
     // THIS ONE FAILS
     // sicut-locutus.ly:98:47: error: syntax error, unexpected '}'
     //                < f g > 2 _\markup { \italic
     //                                              } _\markup { \italic Masked and substituted }  _~ f _~  |
-    //QTest::newRow("sicut-locutus") <<
-    //        "examples" <<
-    //        "sicut-locutus" <<
-    //        defaultOptions;
+    //QTest::newRow("sicut-locutus") << "examples";
 
-    QTest::newRow("stormy-riders") <<
-            "examples" <<
-            "stormy-riders" <<
-            defaultOptions;
-    QTest::newRow("test_tuplets") <<
-            "examples" <<
-            "test_tuplets" <<
-            defaultOptions;
-    QTest::newRow("the-rose-garden") <<
-            "examples" <<
-            "the-rose-garden" <<
-            defaultOptions;
-    QTest::newRow("vivaldi-cs3mv2") <<
-            "examples" <<
-            "vivaldi-cs3mv2" <<
-            defaultOptions;
-    QTest::newRow("vivaldi_op44_11_1") <<
-            "examples" <<
-            "vivaldi_op44_11_1" <<
-            defaultOptions;
+    QTest::newRow("stormy-riders") << "examples";
+    QTest::newRow("test_tuplets") << "examples";
+    QTest::newRow("the-rose-garden") << "examples";
+    QTest::newRow("vivaldi-cs3mv2") << "examples";
+    QTest::newRow("vivaldi_op44_11_1") << "examples";
 
     // data/regression
-    QTest::newRow("export_hidden_key_signatures") <<
-            "regression" <<
-            "export_hidden_key_signatures" <<
-            defaultOptions;
+    QTest::newRow("export_hidden_key_signatures") << "regression";
 }
 
 void TestLilypondExport::testExamples()
 {
+    QString baseName{QTest::currentDataTag()};
     QFETCH(QString, baseDir);
-    QFETCH(QString, baseName);
-    QFETCH(Options, options);
 
     // GIVEN
     const QString input = QFINDTESTDATA("../../data/" + baseDir + "/" + baseName + ".rg");
@@ -332,9 +210,10 @@ void TestLilypondExport::testExamples()
     const QString fileName = baseName + ".ly";
     qDebug() << "Loading" << input << "and exporting to" << fileName;
 
+    // ??? Again, why over and over.  Why not just at init?
     QSettings settings;
     settings.beginGroup(LilyPondExportConfigGroup);
-    settings.setValue("lilyexportbeamings", (options & ExportBeaming) ? true : false);
+    settings.setValue("lilyexportbeamings", true);
     settings.endGroup();
 
     RosegardenDocument doc(nullptr, {}, true /*skip autoload*/, true, false /*no sequencer*/);
