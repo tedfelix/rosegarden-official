e62e6e47774e62afe0939db33b9f136edfd4d64e
Ted Felix <ted@tedfelix.com>
2022-02-17 09:02:16 -0500

    Use QString::endsWith(), even better

--- src/sound/AudioFileManager.cpp
+++ src/sound/AudioFileManager.cpp
@@ -62,7 +62,7 @@ namespace
         QString path2 = path;
 
         // Add a trailing "/" if needed.
-        if (path2.at(path2.size()-1) != '/')
+        if (!path2.endsWith('/'))
             path2 += "/";
 
         return path2;
