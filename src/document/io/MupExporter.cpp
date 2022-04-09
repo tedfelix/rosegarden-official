26a9777670ccbd4c39859551cb67a322c3e92850
Ted Felix <ted@tedfelix.com>
2022-04-09 16:10:33 -0400

    Cleanup

--- src/document/io/MupExporter.cpp
+++ src/document/io/MupExporter.cpp
@@ -16,8 +16,7 @@
 */
 
 #define RG_MODULE_STRING "[MupExporter]"
-// Turn off RG_DEBUG output.
-//#define RG_NO_DEBUG_PRINT
+#define RG_NO_DEBUG_PRINT
 
 #include "MupExporter.h"
 
--- src/gui/seqmanager/ChannelManager.cpp
+++ src/gui/seqmanager/ChannelManager.cpp
@@ -16,7 +16,7 @@
 */
 
 #define RG_MODULE_STRING "[ChannelManager]"
-//#define RG_NO_DEBUG_PRINT 1
+#define RG_NO_DEBUG_PRINT
 
 #include "ChannelManager.h"
 
