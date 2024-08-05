33697b314607bfb3ee2a45d3ec8af772b19dd92c
Ted Felix <ted@tedfelix.com>
2024-08-05 15:04:47 -0400

    ListEditView: Comments

--- src/gui/general/ListEditView.h
+++ src/gui/general/ListEditView.h
@@ -28,20 +28,6 @@ namespace Rosegarden
 
 /// Base class for EventView and TempoView.
 /**
- * ListEditView is a subclass of EditViewBase that contains some
- * functionality used by "old-style" EditView implementations but not
- * required by the new QGraphicsView-based notation and matrix views.
- *
- * The main thing this code implements is a refresh-on-repaint
- * mechanism through paintEvent.  This mechanism is much too complex
- * to be really justified for the only classes that use it now -- it
- * was original designed for more complex views such as the old
- * notation view -- but it's retained more or less verbatim "for
- * historical reasons" awaiting a possible future refactoring.
- *
- * Apart from that, it also provides some widget layout skeleton code,
- * and a few utility methods.
- *
  * Currently the subclasses of this class are EventView and TempoView,
  * which is why it is named ListEditView -- because it's used by list
  * edit views, not because it provides anything particularly focused
@@ -63,6 +49,7 @@ public:
     //     is needed by all.  That could be pushed up to EditViewBase.
 
     // SegmentObserver
+    // ??? Push down to EventView along with ctor/dtor support.
     void segmentDeleted(const Segment *) override;
 
 protected:
