tedfelix/rosegarden-official

commit c9416fb1388e2b49e4fc07b111e72e0f535664c0

EditViewBase: Cleanup

src/document/RosegardenDocument.cpp
@@ -62,7 +62,7 @@
 #include "gui/dialogs/UnusedAudioSelectionDialog.h"
 #include "gui/editors/segment/compositionview/AudioPeaksThread.h"
 #include "gui/editors/segment/TrackLabel.h"
-#include "gui/general/EditViewBase.h"
+//#include "gui/general/EditViewBase.h"
 #include "gui/general/GUIPalette.h"
 #include "gui/general/ResourceFinder.h"
 #include "gui/widgets/StartupLogo.h"

src/document/io/LilyPondExporter.h
@@ -34,7 +34,6 @@
 #include "base/Segment.h"
 #include "base/Selection.h"
 #include "document/io/LilyPondLanguage.h"
-#include "gui/editors/notation/NotationView.h"
 #include <fstream>
 #include <set>
 #include <string>

src/gui/configuration/MatrixConfigurationPage.cpp
@@ -21,7 +21,6 @@
 #include "misc/ConfigGroups.h"
 #include "document/RosegardenDocument.h"
 #include "misc/ConfigGroups.h"
-#include "gui/editors/matrix/MatrixView.h"
 #include "TabbedConfigurationPage.h"
 #include <QSettings>
 #include <QFrame>

src/gui/editors/eventlist/EventView.cpp
@@ -1252,7 +1252,11 @@ EventView::slotClearSelection()
 void
 EventView::setupActions()
 {
-    setupBaseActions(true);
+    setupBaseActions();
+
+    createAction("edit_cut", SLOT(slotEditCut()));
+    createAction("edit_copy", SLOT(slotEditCopy()));
+    createAction("edit_paste", SLOT(slotEditPaste()));
 
     createAction("insert", SLOT(slotEditInsert()));
     createAction("delete", SLOT(slotEditDelete()));

src/gui/editors/eventlist/EventView.h
@@ -74,13 +74,6 @@ signals:
     /// Connected to RosegardenMainViewWidget::slotEditTriggerSegment().
     void editTriggerSegment(int);
 
-protected slots:
-
-    // EditViewBase overrides.
-    void slotEditCut() override;
-    void slotEditCopy() override;
-    void slotEditPaste() override;
-
 protected:
 
     void initStatusBar();
@@ -92,6 +85,9 @@ protected:
 
 private slots:
 
+    void slotEditCut();
+    void slotEditCopy();
+    void slotEditPaste();
     void slotEditDelete();
     void slotEditInsert();
 

src/gui/editors/matrix/MatrixView.cpp
@@ -371,7 +371,11 @@ void
 MatrixView::setupActions()
 {
 
-    setupBaseActions(true);
+    setupBaseActions();
+
+    createAction("edit_cut", SLOT(slotEditCut()));
+    createAction("edit_copy", SLOT(slotEditCopy()));
+    createAction("edit_paste", SLOT(slotEditPaste()));
 
     createAction("select", SLOT(slotSetSelectTool()));
     createAction("draw", SLOT(slotSetPaintTool()));

src/gui/editors/matrix/MatrixView.h
@@ -169,9 +169,9 @@ protected slots:
     void slotUpdateMenuStates();
     void slotRulerSelectionUpdate();
 
-    void slotEditCut() override;
-    void slotEditCopy() override;
-    void slotEditPaste() override;
+    void slotEditCut();
+    void slotEditCopy();
+    void slotEditPaste();
     void slotEditDelete();
 
     /// Show or hide rulers

src/gui/editors/notation/NotationView.cpp
@@ -617,7 +617,11 @@ void
 NotationView::setupActions()
 {
     //setup actions common to all views.
-    EditViewBase::setupBaseActions(true);
+    EditViewBase::setupBaseActions();
+
+    createAction("edit_cut", SLOT(slotEditCut()));
+    createAction("edit_copy", SLOT(slotEditCopy()));
+    createAction("edit_paste", SLOT(slotEditPaste()));
 
     //"file" MenuBar menu
     // "file_save"

commit 48ab1b94fa54f63cbfadaa6e5238a583ba38dea0

Fix crash introduced by c9416fb1

src/document/RosegardenDocument.cpp
@@ -62,7 +62,10 @@
 #include "gui/dialogs/UnusedAudioSelectionDialog.h"
 #include "gui/editors/segment/compositionview/AudioPeaksThread.h"
 #include "gui/editors/segment/TrackLabel.h"
-//#include "gui/general/EditViewBase.h"
+// !!! DO NOT REMOVE THIS!
+//     qDeleteAll() needs this in order to do its work correctly.
+//     Without EditViewBase.h, we crash on close whenever an editor is up.
+#include "gui/general/EditViewBase.h"
 #include "gui/general/GUIPalette.h"
 #include "gui/general/ResourceFinder.h"
 #include "gui/widgets/StartupLogo.h"