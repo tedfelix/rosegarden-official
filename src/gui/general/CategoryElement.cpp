b220b4802d4649168c0091d15b1007e9c2616360
Ted Felix <ted@tedfelix.com>
2025-05-12 22:07:05 -0400

    Turn off some noisy logging

--- src/gui/general/CategoryElement.cpp
+++ src/gui/general/CategoryElement.cpp
@@ -16,6 +16,7 @@
 */
 
 #define RG_MODULE_STRING "[CategoryElement]"
+#define RG_NO_DEBUG_PRINT
 
 #include "CategoryElement.h"
 
--- src/gui/general/PresetHandlerDialog.cpp
+++ src/gui/general/PresetHandlerDialog.cpp
@@ -16,6 +16,7 @@
 */
 
 #define RG_MODULE_STRING "[PresetHandlerDialog]"
+#define RG_NO_DEBUG_PRINT
 
 #include "PresetHandlerDialog.h"
 
