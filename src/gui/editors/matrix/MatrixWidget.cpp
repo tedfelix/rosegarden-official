853313edcf08147d96293cf221299847cca41ada
Ted Felix <ted@tedfelix.com>
2026-01-14 23:33:02 -0500

    matrix: Fix for crash with large hzoom
    Bug #1761

--- src/gui/editors/matrix/MatrixWidget.cpp
+++ src/gui/editors/matrix/MatrixWidget.cpp
@@ -16,7 +16,7 @@
 */
 
 #define RG_MODULE_STRING "[MatrixWidget]"
-#define RG_NO_DEBUG_PRINT 1
+#define RG_NO_DEBUG_PRINT
 
 #include "MatrixWidget.h"
 
@@ -139,8 +139,7 @@ MatrixWidget::MatrixWidget(bool drumMode) :
 {
     //RG_DEBUG << "ctor";
 
-    m_layout = new QGridLayout;
-    setLayout(m_layout);
+    m_layout = new QGridLayout(this);
 
     // Remove thick black lines between rulers and matrix
     m_layout->setSpacing(0);
@@ -273,6 +272,9 @@ MatrixWidget::MatrixWidget(bool drumMode) :
     // This will be done in setSegments().
 
     // Move the scroll bar from m_view to MatrixWidget
+    // ??? Oddly, removing these two lines seems to have no effect on anything.
+    //     Might want to do some careful testing and remove these.  They seem
+    //     very strange.
     m_view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_layout->addWidget(m_view->horizontalScrollBar(),
                         HSCROLLBAR_ROW, MAIN_COL, 1, 1);
@@ -515,10 +517,8 @@ MatrixWidget::setSegments(RosegardenDocument *document,
         m_changerWidget->hide();
     }
 
-    // ??? Interim workaround for #1761?  Obnoxious, but at least it avoids
-    //     the potential crash.
-    if (segments[0]->matrixHZoomFactor > 10)
-        segments[0]->matrixHZoomFactor = 10;
+    // Fix for bug #1761.  See setHorizontalZoomFactor() for more.
+    show();
 
     // Go with zoom factors from the first Segment.
     setHorizontalZoomFactor(segments[0]->matrixHZoomFactor);
@@ -673,35 +673,16 @@ MatrixWidget::setHorizontalZoomFactor(double factor)
     // about the only sensible thing we can do is keep the keyboard scaled at
     // 1.0 horizontally, and only scale it vertically.  Git'r done.
 
-    RG_DEBUG << "setHorizontalZoomFactor():" << factor;
-    RG_DEBUG << "  MatrixView geo before:" << parentWidget()->geometry();
-    // This shows a suspicious massive min size when there is trouble.
-    // MatrixView min before: QSize(1726, 337)
-    RG_DEBUG << "  MatrixView min before:" << parentWidget()->minimumSize();
-    RG_DEBUG << "  MatrixWidget geo before:" << geometry();
-    RG_DEBUG << "  MatrixWidget min before:" << minimumSize();
-    RG_DEBUG << "  MatrixWidget layout geo before:" << m_layout->geometry();
-    // This shows a suspicious massive min size when there is trouble.
-    // MatrixWidget layout min before: QSize(1726, 229)
-    RG_DEBUG << "  MatrixWidget layout min before:" << m_layout->minimumSize();
-    RG_DEBUG << "  Panned geo before:" << m_view->geometry();
-    RG_DEBUG << "  Panned min before:" << m_view->minimumSize();
-
     m_hZoomFactor = factor;
+    // We need to do a show() here at startup to make sure things don't get
+    // badly hosed.  See bug #1761.  But this is overkill.  Calling show()
+    // before we call setHorizontalZoomFactor() in setSegments() is good
+    // enough.  See setSegments().
+    //show();
     if (m_referenceScale)
         m_referenceScale->setXZoomFactor(m_hZoomFactor);
     m_view->resetTransform();
     m_view->scale(m_hZoomFactor, m_vZoomFactor);
-
-    // These are never different from the befores above.
-    RG_DEBUG << "  MatrixView geo after:" << parentWidget()->geometry();
-    RG_DEBUG << "  MatrixView min after:" << parentWidget()->minimumSize();
-    RG_DEBUG << "  MatrixWidget geo after:" << geometry();
-    RG_DEBUG << "  MatrixWidget min after:" << minimumSize();
-    RG_DEBUG << "  MatrixWidget layout geo after:" << m_layout->geometry();
-    RG_DEBUG << "  MatrixWidget layout min after:" << m_layout->minimumSize();
-    RG_DEBUG << "  Panned geo after:" << m_view->geometry();
-    RG_DEBUG << "  Panned min after:" << m_view->minimumSize();
 
     // Only vertical zoom factor is applied to pitch ruler
     QTransform m;
