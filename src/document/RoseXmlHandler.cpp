tedfelix/rosegarden-official

commit be6c1525f2482257f486309834519a8118195b79

Fix matrix velocity forward compatibility
Previously, files saved with the latest could not be loaded with
25.12 due to confusion over the <velocity> tag.  This change
moves matrix velocity to an attrbute of the matrix tag.  Now
25.12 can load current format files.

See Will's message on the user's list:
https://sourceforge.net/p/rosegarden/mailman/message/59311561/

src/document/RoseXmlHandler.cpp
@@ -1152,8 +1152,10 @@ RoseXmlHandler::startElement(const QString& namespaceURI,
     } else if (lcName == "matrix") {  // <matrix>
 
         // If we're in a <segment>, <matrix> is valid.
-        if (m_currentSegment)
+        if (m_currentSegment) {
             m_inMatrix = true;
+            m_currentSegment->matrixVelocity = atts.value("velocity").toUInt();
+        }
 
     } else if (lcName == "notation") {  // <notation>
 
@@ -1170,11 +1172,6 @@ RoseXmlHandler::startElement(const QString& namespaceURI,
 
         if (m_currentSegment && m_inMatrix)
             m_currentSegment->matrixVZoomFactor = atts.value("factor").toDouble();
-
-    } else if (lcName == "velocity") {  // <velocity>
-
-        if (m_currentSegment && m_inMatrix)
-            m_currentSegment->matrixVelocity = atts.value("value").toUInt();
 
     } else if (lcName == "ruler") {  // <ruler>
 

src/document/RosegardenDocument.cpp
@@ -1554,14 +1554,17 @@ void RosegardenDocument::saveSegment(QTextStream &outStream,
 
         // <matrix>
 
-        outStream << "  <matrix>\n";
+        outStream << "  <matrix";
+        // Going with an attribute of <matrix> due to a conflict with
+        // other <velocity> tag handling that causes forward compatibility
+        // issues.
+        outStream << " velocity=\"" << segment->matrixVelocity << "\"";
+        outStream << ">\n";
 
         // Zoom factors
         outStream << "    <hzoom factor=\"" << segment->matrixHZoomFactor <<
                      "\" />\n";
         outStream << "    <vzoom factor=\"" << segment->matrixVZoomFactor <<
-                     "\" />\n";
-        outStream << "    <velocity value=\"" << segment->matrixVelocity <<
                      "\" />\n";
 
         // For each matrix ruler...

commit bbbcf9e605eb4ad3bc0cd2e265b9ac7a40e6ae96

Fix open failure on recent format .rg files
This was caused by [be6c1525f2].

src/document/RoseXmlHandler.cpp
@@ -1810,31 +1810,28 @@ RoseXmlHandler::startElement(const QString& namespaceURI,
             m_deprecation = true;
         }
 
-        if (m_section != InInstrument) {
-            m_errorString = "Found Volume outside Instrument";
-            return false;
-        }
+        if (m_section == InInstrument) {
+            MidiByte value = atts.value("value").toInt();
 
-        MidiByte value = atts.value("value").toInt();
-
-        if (m_instrument) {
-            if (m_instrument->getType() == Instrument::Midi) {
-                // If we've not encountered a volume CC, go with this.
-                if (!m_haveVolumeCC) {
-                    m_instrument->setControllerValue(
-                            MIDI_CONTROLLER_VOLUME, value);
+            if (m_instrument) {
+                if (m_instrument->getType() == Instrument::Midi) {
+                    // If we've not encountered a volume CC, go with this.
+                    if (!m_haveVolumeCC) {
+                        m_instrument->setControllerValue(
+                                MIDI_CONTROLLER_VOLUME, value);
+                    }
+                } else {
+                    // For Audio and SoftSynth Instruments, translate into level.
+                    // Backward compatibility: "volume" was in a 0-127
+                    // range and we now store "level" (float dB) instead.
+                    // Note that we have no such compatibility for
+                    // "recordLevel", whose range has changed silently.
+                    if (!m_deprecation)
+                        RG_WARNING << "WARNING: This Rosegarden file uses the deprecated element \"volume\" for an audio instrument (now replaced by \"level\").  We recommend re-saving the file from this version of Rosegarden to assure your ability to re-load it in future versions";
+                    m_deprecation = true;
+                    m_instrument->setLevel
+                        (AudioLevel::multiplier_to_dB(float(value) / 100.0));
                 }
-            } else {
-                // For Audio and SoftSynth Instruments, translate into level.
-                // Backward compatibility: "volume" was in a 0-127
-                // range and we now store "level" (float dB) instead.
-                // Note that we have no such compatibility for
-                // "recordLevel", whose range has changed silently.
-                if (!m_deprecation)
-                    RG_WARNING << "WARNING: This Rosegarden file uses the deprecated element \"volume\" for an audio instrument (now replaced by \"level\").  We recommend re-saving the file from this version of Rosegarden to assure your ability to re-load it in future versions";
-                m_deprecation = true;
-                m_instrument->setLevel
-                    (AudioLevel::multiplier_to_dB(float(value) / 100.0));
             }
         }
 