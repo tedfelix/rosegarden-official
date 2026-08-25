5f48807225b09f0cba85c2a9c52049b6e1b558f8
Ted Felix <ted@tedfelix.com>
2026-08-25 10:26:56 -0400

    TrackButtons: Clean up debug logging

--- src/gui/editors/segment/TrackButtons.cpp
+++ src/gui/editors/segment/TrackButtons.cpp
@@ -15,6 +15,7 @@
     COPYING included with this distribution for more information.
 */
 
+#define RG_NO_DEBUG_PRINT
 
 #include "TrackButtons.h"
 
@@ -474,7 +475,7 @@ TrackButtons::removeButtons(int position)
     //RG_DEBUG << "removeButtons() - deleting track button at position:" << position;
 
     if (position < 0  ||  position >= m_tracks) {
-        RG_DEBUG << "%%%%%%%%% BIG PROBLEM : TrackButtons::removeButtons() was passed a non-existing index\n";
+        RG_WARNING << "%%%%%%%%% BIG PROBLEM : TrackButtons::removeButtons() was passed a non-existing index\n";
         return;
     }
 
@@ -549,16 +550,16 @@ TrackButtons::slotUpdateTracks()
                     m_trackHBoxes.push_back(trackHBox);
                 }
             } else
-                RG_DEBUG << "TrackButtons::slotUpdateTracks - can't find TrackId for position " << i;
+                RG_WARNING << "TrackButtons::slotUpdateTracks - can't find TrackId for position " << i;
         }
     }
 
     m_tracks = newNbTracks;
 
     if (m_tracks != (int)m_trackHBoxes.size())
-        RG_DEBUG << "WARNING  TrackButtons::slotUpdateTracks(): m_trackHBoxes.size() != m_tracks";
+        RG_WARNING << "WARNING  TrackButtons::slotUpdateTracks(): m_trackHBoxes.size() != m_tracks";
     if (m_tracks != (int)m_trackLabels.size())
-        RG_DEBUG << "WARNING  TrackButtons::slotUpdateTracks(): m_trackLabels.size() != m_tracks";
+        RG_WARNING << "WARNING  TrackButtons::slotUpdateTracks(): m_trackLabels.size() != m_tracks";
 
     // For each track
     for (int i = 0; i < m_tracks; ++i) {
@@ -1338,7 +1339,7 @@ TrackButtons::getRecordLedColour(Instrument *ins)
 
     case Instrument::InvalidInstrument:
     default:
-        RG_DEBUG << "TrackButtons::slotUpdateTracks() - invalid instrument type, this is probably a BUG!";
+        RG_WARNING << "TrackButtons::slotUpdateTracks() - invalid instrument type, this is probably a BUG!";
         return Qt::green;
 
     }
